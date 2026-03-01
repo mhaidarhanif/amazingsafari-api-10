fa655fdd8cab853436d8cbf7decfc3d0a71da090
M Haidar Hanif <git@mhaidarhanif.com>
2026-03-01 17:50:45 +0700

    feat: cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
+import { cors } from "hono/cors";
 
 import { productRoute } from "./modules/product/route";
 import { commonRoute } from "./modules/common/route";
@@ -7,6 +8,8 @@ import { commonRoute } from "./modules/common/route";
 const app = new Hono();
 
 app.use(logger());
+app.use(cors());
+
 app.route("/", commonRoute);
 app.route("/products", productRoute);
 
